a9a30024e3135082a21755e80031e22e8908ebbd
riyadmahmudkhan <mdriyadhossain356@gmail.com>
2023-04-03 02:19:36 +0600

    add navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home components</h1>
+            <button className="btn btn-secondary">Button</button>
+        </div>
+    );
+};
+
+export default Home;
